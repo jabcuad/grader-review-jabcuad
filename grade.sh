62fa4415f508df4d3eae2a97836ecae535eace31
jabcuad <146897326+jabcuad@users.noreply.github.com>
2023-11-07 17:47:37 -0800

    hh

--- grade.sh
+++ grade.sh
@@ -8,7 +8,27 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+cd student-submission
 
+if [[ -f ListExamples.java ]]
+then
+echo "correct file"
+else
+echo"wrong file"
+exit 1
+fi
+
+cd ..
+cp student-submission/*.java grading-area
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > error.txt 
+
+
+if [[ -s error.txt ]] 
+then 
+echo "There's an error"
+fi
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
